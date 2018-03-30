5798dc7a03c391d6a8c9dba958c6fe2c89a294bd
yotsaphonSutweha <komsan_58@hotmail.com>
2018-03-30 23:20:03 +0100

    refactoring file structure

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
+
+require('./services/passport')
 const app = express();
 
 app.get('/', (req, res) => {
     res.send({Hi: 'there'});
 });
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleID,
-            clientSecret: keys.googleSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
-        }
-    )
-)
 
-app.get('/auth/google', passport.authenticate('google',{
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+require('./routes/authRoutes')(app);
 
 const PORT = 8080;
 app.listen(PORT);
