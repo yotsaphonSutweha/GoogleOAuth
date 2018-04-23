de87fe258d01fb7a76df6fc77f10f73c562bfab6
yotsaphonSutweha <komsan_58@hotmail.com>
2018-04-23 21:49:24 +0100

    if statement checking if the user is already exists

--- services/passport.js
+++ services/passport.js
@@ -13,10 +13,24 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            // create a new model instance and save it to the database
-            // profile.id is the id coming from the user's profile
-            // .save is the method that persist the user's record into the database
-            new User({ googleId: profile.id }).save();
+            // look through the record and search for the google.id inside the database
+            // checking if the user is existed 
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if(existingUser) {
+                        // we already have a record with the given profile ID
+
+                    }else{
+                        // we don't have a record with this ID, make a new record 
+                                    
+                        // create a new model instance and save it to the database
+                        // profile.id is the id coming from the user's profile
+                        // .save is the method that persist the user's record into the database
+                        new User({ googleId: profile.id }).save();
+                    }
+                })
+            
+          
         }
     )
 )
